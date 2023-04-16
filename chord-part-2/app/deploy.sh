0012fd8168cc6f3797c5a21480b9787a4e4a9919
soyccan <soyccan@gmail.com>
2023-04-16 20:31:52 +0800

    Chord Part II - Config Template
    Since chord-leader and chord deployment only differ in small parts,
    we use a config template to generate them.
    
    TODO: use kustomize
    
    Rename scripts to use dash instead of underscore.

--- chord-part-2/app/deploy.sh
+++ chord-part-2/app/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eux
+set -ux
 
 echo Set your registry url in env: REGISTRY_URL
 
@@ -8,4 +8,24 @@ if [ ! "${REGISTRY_URL:-}" ]; then
     export REGISTRY_URL=$(minikube ip):5000
 fi
 
-envsubst < chord.yaml | kubectl apply -f-
+mkdir -pv .gen
+
+# generate normal chord
+export NAME=chord
+export REPLICAS=2
+export HOSTNAME_SUBDOMAIN=
+export LEADER_ENV=
+envsubst < chord.yaml > .gen/chord.yaml
+
+# generate chord-leader
+export NAME=chord-leader
+export REPLICAS=1
+export HOSTNAME_SUBDOMAIN='hostname: leader
+      subdomain: chord'
+export LEADER_ENV='- { name: LEADER, value: "true" }'
+envsubst < chord.yaml > .gen/chord-leader.yaml
+
+# apply
+kubectl apply -f .gen/chord.yaml
+kubectl apply -f .gen/chord-leader.yaml
+kubectl apply -f chord-svc.yaml
